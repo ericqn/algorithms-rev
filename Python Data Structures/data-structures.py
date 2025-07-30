a17959dd57e1a6e0a3aa0cf16eaa20e1bc32927e
ericqn <ericqn1234@gmail.com>
2025-07-29 18:08:33 -0700

    added binary search algorithm and testing

--- Python Data Structures/data-structures.py
+++ Python Data Structures/data-structures.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 class Graph_Node:
     def __init__(self, id:str):
         self.id = id
@@ -52,7 +54,7 @@ class Graph:
             
             self.edges += edge
     
-    # TODO:
+    # TODO: Docstring and check implementation.
     def remove_edges(self, edges:list):
         for edge in edges:
             if edge[0] not in self.nodes.keys() or edge[1] not in self.nodes.keys():
@@ -180,7 +182,27 @@ class BST:
         print(curr_node)
         self.inorder_traversal(curr_node.right)
 
+def binary_search(nums: list[int], target: int, min_idx: int=None, max_idx: int=None):
+    """
+    Input list should be sorted. Returns the index of the value desired. 
+    If the target value does not exist, returns None.
+    """
+    if min_idx is None: min_idx = 0
+    if max_idx is None: max_idx = len(nums) - 1
+    while min_idx <= max_idx:
+        mid_idx = min_idx + (max_idx - min_idx) // 2
+
+        if nums[mid_idx] == target: 
+            return mid_idx
+        elif nums[mid_idx] < target:
+            min_idx = mid_idx + 1
+        else:
+            max_idx = mid_idx - 1
+        
+    return None
+
     
+
 if __name__ == '__main__':
     node1 = BST_Node(3)
     node2 = BST_Node(7)
@@ -209,3 +231,11 @@ if __name__ == '__main__':
     graph.breadth_first_search('C')
     graph.breadth_first_search('F')
     graph.breadth_first_search('E')
+
+    print('=== BINARY SEARCH TESTING ===')
+    nums_list = [-39,-18,-5,-3,-1,0,1,2,3,5,16,21,28]
+    nums_list_2 = sorted(np.random.randint(low=-500, high=500, size=100))
+    
+    target_idx = binary_search(nums_list, target=30)
+    assert target_idx == None
+
